aed2ab9d922be11a25b97eb494ee9a18f418665d
Dandara Mariama <dandaramariama@gmail.com>
2026-09-19 20:26:22 -0400

    All done :)

--- hw01.py
+++ hw01.py
@@ -104,7 +104,9 @@ def main():
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
-
+    result4 = a % b
+    #print
+    print(f"Part 4: result = {result4}")
     # End of Part 4 ----------------------
 
 if __name__ == "__main__":
